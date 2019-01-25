6b743c57fa8075f2c60c61ca7c8fa72824dcaa40
Rafaiejokandan <req93271@c02pvt35g8wp.corp.mutualofomaha.com>
2019-01-25 12:05:13 -0600

    Update the install_lib for all the platforms

--- install_lib.bat
+++ install_lib.bat
@@ -1,18 +1,17 @@
-#!/bin/bash
 
-# It eases the installation of the libs on Mac and Linux 
+rem It eases the installation of the libs on Mac and Linux 
 
-# install main libs
+rem install main libs
 pip install numpy pandas scikit-learn
 pip install gym
 
-# install maze lib
-mkdir -p tmp
+rem install maze lib
+md -p tmp
 cd tmp
 git clone https://github.com/rafaie/gym-maze
 cd gym-maze
 python setup.py install
 cd ../..
-rm -rf tmp/gym-maze  && rm -rf tmp
+rem rm -rf tmp/gym-maze  && rm -rf tmp
 
 pip freeze
