2fb2cc3a67fae14986736709b14d58e10313ec7a
Rafaiejokandan <req93271@c02pvt35g8wp.corp.mutualofomaha.com>
2019-02-13 09:14:58 -0600

    init tournament_run.py

---
+++ tournament_run.py
@@ -0,0 +1,16 @@
+"""
+
+tournament_run.py: run the tournament 
+
+"""
+
+__copyright__ = "Copyright 2018, MoO"
+__license__ = ""
+__author__ = "Mostafa Rafaie"
+__maintainer__ = "Mostafa Rafaie"
+
+
+
+
+if __name__ == "__main__":
+    pass
