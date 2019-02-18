1082091485d12d68230557cc12db9a2f1227e6c1
Rafaiejokandan <req93271@c02pvt35g8wp.corp.mutualofomaha.com>
2019-02-18 00:16:11 -0600

    Add tournament_run class

--- tournament_run.py
+++ tournament_run.py
@@ -4,13 +4,25 @@ tournament_run.py: run the tournament
 
 """
 
-__copyright__ = "Copyright 2018, MoO"
+__copyright__ = "Copyright 2019, MoO"
 __license__ = ""
 __author__ = "Mostafa Rafaie"
 __maintainer__ = "Mostafa Rafaie"
 
+import sys
+from single_run import Simulator
 
 
+class TournamentSimulator(Simulator):
 
-if __name__ == "__main__":
-    pass
+    # Initialize the class 
+    def __init__(self, maze, num_episodes, min_episodes, max_t, 
+                 render_maze, enable_recording, debug_mode=False):
+        super().__init__(agent_name=None, maze=maze, num_episodes=num_episodes,
+                         min_episodes=min_episodes, max_t=max_t, render_maze=render_maze,
+                         enable_recording=enable_recording, debug_mode=debug_mode,
+                         need_agent=False)
+
+    def run(self, agents):
+        pass
+ 
