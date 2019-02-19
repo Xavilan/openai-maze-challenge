406d1de79cdb3ebd35bb1a869d75545e01785a24
Rafaiejokandan <req93271@c02pvt35g8wp.corp.mutualofomaha.com>
2019-02-19 11:59:44 -0600

    Update install batch scripts

--- install_lib.bat
+++ install_lib.bat
@@ -1,10 +1,9 @@
 REM It eases the installation of the libs on Mac and Linux 
 
 REM install main libs
-pip install numpy pandas scikit-learn
-pip install gym
+conda install numpy pandas scikit-learn
+conda install gym
 
 REM install maze lib
 pip install git+https://github.com/rafaie/gym-maze
 
-pip freeze
--- install_lib.sh
+++ install_lib.sh
@@ -3,10 +3,8 @@
 # It eases the installation of the libs on Mac and Linux 
 
 # install main libs
-pip install numpy pandas scikit-learn
-pip install gym
+conda install numpy pandas scikit-learn
+conda install gym
 
 # install maze lib
 pip install git+https://github.com/rafaie/gym-maze
-
-pip freeze
