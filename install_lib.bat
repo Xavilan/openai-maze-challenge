d5ab78a1674b714e92e580d41f36a26d92c6a797
Mostafa Rafaiejokandan <mostafa.rafaiejokandan@mutualofomaha.com>
2019-02-25 15:42:07 -0600

    Abnormal error caused to have this change!

--- install_lib.bat
+++ install_lib.bat
@@ -1,9 +1,3 @@
-REM It eases the installation of the libs on Mac and Linux 
-
-REM install main libs
-conda install numpy pandas scikit-learn
-conda install gym
-
-REM install maze lib
+pip install gym
 pip install git+https://github.com/rafaie/gym-maze
-
+conda install numpy pandas scikit-learn
