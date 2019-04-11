76f6d8106190fed60fee446306faf58935d8c9ff
Dan Freimund <daniel.freimund@mutualofomaha.com>
2019-04-11 15:44:54 -0500

    Throwing my hat in the ring

---
+++ agents/Xavilan.py
@@ -0,0 +1,184 @@
+"""
+
+Xavilan.py: Dan Freimund's agent started from an example agent
+               It's a new version driven from below example:
+               https://github.com/MattChanTK/ai-gym/blob/master/maze_2d/maze_2d_q_learning.py
+
+"""
+
+__copyright__ = "Copyright 2018, MoO"
+__license__ = ""
+__author__ = "Dan Freimund"
+__maintainer__ = "Dan Freimund"
+
+from agents.base_agent import BaseAgent
+import numpy as np
+import math
+import random
+
+class Xavilan(BaseAgent):
+    
+
+    def __init__(self, maze, num_episodes, observation_space_low, observation_space_high, 
+                 observation_space_shape, space_action_n, max_t, debug_mode):
+        super().__init__(maze, num_episodes, observation_space_low, observation_space_high, 
+                        observation_space_shape, space_action_n, max_t, debug_mode)
+        self.init_example_agent()
+
+
+    def init_example_agent(self):
+        '''
+        Creating a Q-Table for each state-action pair
+        '''
+
+        # Create a tuple with the maze size. As an example for the Random 10x10 random maze, it works in this way:
+        # self.observation_space_high is a list [9, 9] ( I guess it's not [10, 10] as the first value of the array is started from zero)
+        # self.observation_space_shape is a tuple showing the dimension of the maze. for this example, it's (2,)
+        # and finally, self.maze_size is converted to actual size [10, 10]!
+        self.maze_size = tuple((self.observation_space_high + np.ones(self.observation_space_shape)).astype(int))
+        # Refers to the possible states with the actual dimension. For this example, it's [10, 10] 
+        self.num_buckets = self.maze_size
+        # Create q-table which is used in the Q-Learning algorithm.
+        # It builds a multi-dimensional array derived from maze dimension + possible actions.
+        # For this example, it establishes a 3-dimensional array with this size [10, 10, 4].
+        self.q_table = np.zeros(self.maze_size + (self.space_action_n,), dtype=float)
+
+        # A variable for keeping the minimum exploration rate and learning rate
+        self.MIN_EXPLORE_RATE = 0.001
+        self.MIN_LEARNING_RATE = 0.2
+
+        # It's a decay factor to change the exploration and exploitation rate in every step.
+        # for this example, it generates 10.0
+        self.DECAY_FACTOR = np.prod(self.maze_size, dtype=float) / 10.0
+        
+        # It just define the bound of the maze in every dimension
+        # for this example, it generates [(0, 9), (0, 9)]
+        self.state_bounds = list(zip(self.observation_space_low, self.observation_space_high))
+
+        # Instantiating the learning related parameters
+        self.learning_rate = self.get_learning_rate(0)
+        self.explore_rate = self.get_explore_rate(0)
+
+        # A Q-Learning variable 
+        # is a number between 0 and 1 and has the effect of valuing rewards received earlier higher 
+        # than those received later (reflecting the value of a "good start"). It may also be interpreted 
+        # as the probability to succeed (or survive) at every step.
+        # https://en.wikipedia.org/wiki/Q-learning#Discount_factor
+        self.discount_factor = 0.99
+
+        # Keep the number of tries (episodes)
+        self.tries = 0
+
+        # Keep the number of successful tries (episodes)
+        self.num_streaks = 0
+        
+        # Keep the total reward collected
+        self.total_reward = 0
+        
+        # Determine the current state.
+        self.state_0 = None
+        self.SOLVED_T = np.prod(self.maze_size, dtype=int)
+        self.STREAK_TO_END = 100
+
+
+    # It's the function to map the observed state received from the environment
+    # to bucket as the internal dataset keeping the state info. For this example,
+    # there is no need for scaling, but I prefer to use this function as I wasn't
+    # sure if it works for other maze environment or not.
+    def state_to_bucket(self, state):
+        bucket_indice = []
+        for i in range(len(state)):
+            if state[i] <= self.state_bounds[i][0]:
+                bucket_index = 0
+            elif state[i] >= self.state_bounds[i][1]:
+                bucket_index = self.num_buckets[i] - 1
+            else:
+                # Mapping the state bounds to the bucket array
+                bound_width = self.state_bounds[i][1] - self.state_bounds[i][0]
+                offset = (self.num_buckets[i]-1)*self.state_bounds[i][0]/bound_width
+                scaling = (self.num_buckets[i]-1)/bound_width
+                bucket_index = int(round(scaling*state[i] - offset))
+            bucket_indice.append(bucket_index)
+        return tuple(bucket_indice)
+
+
+    # Get the agent information
+    # It's required to be updated by every team.
+    def get_info(self):
+        return { 
+            'team_name': 'Xavilan',
+            'team member': 'Dan Freimund',
+            'version': '1.0.0',
+            'slogan': '',
+            'email': ''
+        }
+    
+    # Inform the agent that a new episode is started
+    # It actually called by simulator informing the agent
+    # the game reset 
+    def reset(self, obv):
+        # Reset the current state and other variables.
+        self.state_0 = self.state_to_bucket(obv)
+        self.done = False
+        self.explore_rate = self.get_explore_rate(self.tries)
+        self.learning_rate = self.get_learning_rate(self.tries)
+        self.tries += 1
+        self.total_reward = 0
+
+    # Request the agent to decide about the action
+    # It's called by the simulator
+    def select_action(self):
+        # Select a random action
+        if random.random() < self.explore_rate:
+            action = int(np.random.uniform(0,4))
+        # Select the action with the highest q from Q-table
+        else:
+            action = int(np.argmax(self.q_table[self.state_0]))
+        return action
+
+    # It's called by the simulator and share the 
+    # information after applying the action by simulator on the environment.
+    def observe(self, obv, reward, done, action):
+        # Convert state observation received from environment to the internal data structure.
+        state = self.state_to_bucket(obv)
+        self.total_reward += reward
+
+        # Update the Q-Table based on the result. 
+        # Actually, it just updates a Q(the previous state + decided action)
+        # It's based on Q-Learning algorithem.
+        # https://en.wikipedia.org/wiki/Q-learning
+        # self.q_table[self.state_0 + (action,)] : It refere to the specific action on a state. 
+        #                                          Don't forgot that that action is the last dimention of the q-table
+        # best_q also returns the q value of the best possible action in the new state.
+        best_q = np.amax(self.q_table[state])
+        self.q_table[self.state_0 + (action,)] += self.learning_rate * (reward + self.discount_factor * (best_q) - self.q_table[self.state_0 + (action,)])
+
+        # Setting up for the next iteration and update the current state
+        self.state_0 = self.state_to_bucket(obv)
+        self.done = done
+    
+    # Give control to stop the episodes if the agent needs!
+    def need_to_stop_episode(self):
+        return False
+
+    # Give control to stop the game if the agent needs!
+    # In the leaderboard, it is controlled by the simulator, and it will not work.
+    def need_to_stop_game(self):
+        if self.done is True:
+            if self.tries <= self.SOLVED_T:
+                self.num_streaks += 1
+            else:
+                self.num_streaks = 0
+
+        if self.num_streaks > self.STREAK_TO_END:
+            return True
+
+        return False
+
+    # Generate an exploration rate dependent on the steps of an episode by using a logarithmic equation
+    def get_explore_rate(self, t):
+        return max(self.MIN_EXPLORE_RATE, min(0.8, 1.0 - math.log10((t+1)/self.DECAY_FACTOR)))
+
+    # Generate a learning rate dependent on the steps of an episode by using a logarithmic equation
+    def get_learning_rate(self, t):
+        return max(self.MIN_LEARNING_RATE, min(0.8, 1.0 - math.log10((t+1)/self.DECAY_FACTOR)))
